1f10496ab026522ddea866ac570fc65bd5b80d6d
Hunter Gregal <huntergregal@yahoo.com>
2014-03-21 11:18:21 -0400

    Slimed down, made more PEP8 compliant.

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ Swag
 ====
 
 Source code for Swag package
+Fun python script to give your linux session some swag.
+
+usage: python swag.py [options]
+
+
+Copyright © 214 Hunter Gregal hunter@huntergregal.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+            
+
+
+
+
+
+
+	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
--- swag.py
+++ swag.py
@@ -11,46 +11,92 @@ import time
 import os
 
 #################Define Functions##########################
+
+
 def s():
-	i=int(0)
-	s=[" .----------------.","| .--------------. |","| |    _______   | |","| |   /  ___  |  | |",
-		"| |  |  (__ \_|  | |","| |   '.___`-.   | |","| |  |`\____) |  | |",
-		"| |  |_______.'  | |","| |              | |","| '--------------' |"," '----------------'"]
-	while (i<=10):
-		print(s[i])
-		time.sleep(0.05)
-		i=i+1
+    i = int(0)
+    s = [
+        " .----------------.",
+        "| .--------------. |",
+        "| |    _______   | |",
+        "| |   /  ___  |  | |",
+        "| |  |  (__ \_|  | |",
+        "| |   '.___`-.   | |",
+        "| |  |`\____) |  | |",
+        "| |  |_______.'  | |",
+        "| |              | |",
+        "| '--------------' |",
+        " '----------------'"]
+    while (i <= 10):
+        print(s[i])
+        time.sleep(0.05)
+        i = i + 1
+
+
 def w():
-	i=int(0)
-	w=["         .----------------.","        | .--------------. |","        | | _____  _____ | |","        | ||_   _||_   _|| |",
-	"        | |  | | /\ | |  | |","        | |  | |/  \| |  | |","        | |  |   /\   |  | |","        | |  |__/  \__|  | |",
-	"        | |              | |","        | '--------------' |","         '----------------'"]
-        while (i<=10):
-                print(w[i])
-                time.sleep(0.05)
-                i=i+1
+    i = int(0)
+    w = [
+        "         .----------------.",
+        "        | .--------------. |",
+        "        | | _____  _____ | |",
+        "        | ||_   _||_   _|| |",
+        "        | |  | | /\ | |  | |",
+        "        | |  | |/  \| |  | |",
+        "        | |  |   /\   |  | |",
+        "        | |  |__/  \__|  | |",
+        "        | |              | |",
+        "        | '--------------' |",
+        "         '----------------'"]
+    while (i <= 10):
+        print(w[i])
+        time.sleep(0.05)
+        i = i + 1
+
+
 def a():
-	i=int(0)
-	a=["                 .----------------.","                | .--------------. |","                | |      __      | |","                | |     /  \     | |",
-	"                | |    / /\ \    | |","                | |   / ____ \   | |","                | | _/ /    \ \_ | |","                | ||____|  |____|| |",
-	"                | |              | |","                | '--------------' |","                 '----------------'"]
-        while (i<=10):
-                print(a[i])
-                time.sleep(0.05)
-                i=i+1
+    i = int(0)
+    a = [
+        "                 .----------------.",
+        "                | .--------------. |",
+        "                | |      __      | |",
+        "                | |     /  \     | |",
+        "                | |    / /\ \    | |",
+        "                | |   / ____ \   | |",
+        "                | | _/ /    \ \_ | |",
+        "                | ||____|  |____|| |",
+        "                | |              | |",
+        "                | '--------------' |",
+        "                 '----------------'"]
+    while (i <= 10):
+        print(a[i])
+        time.sleep(0.05)
+        i = i + 1
+
+
 def g():
-	i=int(0)
-	g=["                         .----------------.","                        | .--------------. |","                        | |    ______    | |",
-	"                        | |  .' ___  |   | |","                        | | / .'   \_|   | |","                        | | | |    ____  | |",
-	"                        | | \ `.___]  _| | |","                        | |  `._____.'   | |","                        | |              | |",
-	"                        | '--------------' |","                         '----------------'",]
-        while (i<=10):
-                print(g[i])
-                time.sleep(0.05)
-                i=i+1
+    i = int(0)
+    g = [
+        "                         .----------------.",
+        "                        | .--------------. |",
+        "                        | |    ______    | |",
+        "                        | |  .' ___  |   | |",
+        "                        | | / .'   \_|   | |",
+        "                        | | | |    ____  | |",
+        "                        | | \ `.___]  _| | |",
+        "                        | |  `._____.'   | |",
+        "                        | |              | |",
+        "                        | '--------------' |",
+        "                         '----------------'",
+    ]
+    while (i <= 10):
+        print(g[i])
+        time.sleep(0.05)
+        i = i + 1
+
+
 def swagB():
-	swagBlocks="""
-	.----------------.  .----------------.  .----------------.  .----------------. 
+    swagBlocks = """
+	.----------------.  .----------------.  .----------------.  .----------------.
 	| .--------------. || .--------------. || .--------------. || .--------------. |
 	| |    _______   | || | _____  _____ | || |      __      | || |    ______    | |
 	| |   /  ___  |  | || ||_   _||_   _|| || |     /  \     | || |  .' ___  |   | |
@@ -61,16 +107,18 @@ def swagB():
 	| |              | || |              | || |              | || |              | |
 	| '--------------' || '--------------' || '--------------' || '--------------' |
 	 '----------------'  '----------------'  '----------------'  '----------------' """
-	i=int(0)
-	while (i<=2):
-		os.system('clear')
-		print(swagBlocks)
-		time.sleep(0.5)
-		os.system('clear')
-		time.sleep(0.5)
-		i=i+1
+    i = int(0)
+    while (i <= 2):
+        os.system('clear')
+        print(swagBlocks)
+        time.sleep(0.5)
+        os.system('clear')
+        time.sleep(0.5)
+        i = i + 1
+
+
 def swag3D():
-	swag3D=r"""
+    swag3D = r"""
 	          _____                    _____                    _____                    _____
 	         /\    \                  /\    \                  /\    \                  /\    \
 	        /::\    \                /::\____\                /::\    \                /::\    \
@@ -92,48 +140,69 @@ def swag3D():
 	       \::::/    /              \::::/    /               /:::/    /              \::::/    /
 	        \::/    /                \::/____/                \::/    /                \::/____/
 	         \/____/                  ~~                       \/____/"""
-	print(swag3D)
+    print(swag3D)
 
 ###########Arguemnts##############
-parser=argparse.ArgumentParser(description='Initiates Swag (Use with Screen or Tmux to always be swagged out)')
-parser.add_argument('-s', '--scroll', help='(Optional) An endless scroll of swag.',action='store_true', required=False)
-parser.add_argument('-b', '--blink', help='(Optional) Ever blinking swag on your screen.',action='store_true', required=False)
-parser.add_argument('-d', '--dimensional', help='(Optional) A 3D swag banner to accompany your work.',action='store_true', required=False)
-parser.add_argument('-l', '--loop', help='(Optional) A continuous loop of all the swag',action='store_true', required=False)
+parser = argparse.ArgumentParser(
+    description='Initiates Swag (Use with Screen or Tmux to always be swagged out)')
+parser.add_argument(
+    '-s',
+    '--scroll',
+    help='(Optional) An endless scroll of swag.',
+    action='store_true',
+    required=False)
+parser.add_argument(
+    '-b',
+    '--blink',
+    help='(Optional) Ever blinking swag on your screen.',
+    action='store_true',
+    required=False)
+parser.add_argument(
+    '-d',
+    '--dimensional',
+    help='(Optional) A 3D swag banner to accompany your work.',
+    action='store_true',
+    required=False)
+parser.add_argument(
+    '-l',
+    '--loop',
+    help='(Optional) A continuous loop of all the swag',
+    action='store_true',
+    required=False)
 args = parser.parse_args()
-########Script Start#########i
+# Script Start#########i
 if (args.scroll):
-	os.system('clear')
-	while (args.scroll):
-		s()
-		w()
-		a()
-		g()
+    os.system('clear')
+    while (args.scroll):
+        s()
+        w()
+        a()
+        g()
 if (args.blink):
-	while (args.blink):
-		swagB()
+    while (args.blink):
+        swagB()
 if (args.dimensional):
-	os.system('clear')
-	swag3D()
-	time.sleep(999999)
+    os.system('clear')
+    swag3D()
+    time.sleep(999999)
 if (args.loop):
-	while (args.loop):
-		os.system('clear')
-		s()
-		w()
-		a()
-		g()
-		swagB()
-		swag3D()
-	
+    while (args.loop):
+        os.system('clear')
+        s()
+        w()
+        a()
+        g()
+        swagB()
+        swag3D()
+
 else:
-	os.system('clear')
-	s()
-	w()
-	a()
-	g()
-	swagB()
-	swag3D()
+    os.system('clear')
+    s()
+    w()
+    a()
+    g()
+    swagB()
+    swag3D()
 
 
 ##########################################
